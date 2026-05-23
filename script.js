1b5a672fc5de49ff39472d3a67094cdb2759e208
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2026-05-23 09:45:47 +0000

    added the character image

--- script.js
+++ script.js
@@ -1,45 +1,115 @@
 const canvas = document.querySelector("canvas")
+const c = canvas.getContext('2d');
+const chr = document.querySelector("#chr")
 
-const c = canvas.getContext('2d')
+canvas.height = innerHeight;
+canvas.width = innerWidth;
 
-canvas.height = 700;
-canvas.width = 576;
-
-const g=0.4;
+const g=1;
 
 class Character{
     constructor(position){
         this.position=position;
-        this.speed = 1;
+        this.speed = {
+            x:0,
+            y:1
+        }
+        this.width=80;
+        this.height=150;
     }
 
     draw(){
-        c.fillStyle='red';
-        c.fillRect(this.position.x,this.position.y,50,150)
+        c.drawImage(chr,this.position.x,this.position.y,this.width,this.height)
     }
 
     update(){
         this.draw();
-        this.position.y +=this.speed;
+        this.position.x +=this.speed.x;
+        this.position.y +=this.speed.y;
 
-        if(this.position.y+150 >= canvas.height){
-            this.speed = 0;
+        if(this.position.y+this.height >= canvas.height){
+            this.speed.y = 0;
         }
         else{
-            this.speed += g;
+            this.speed.y += g;
         }
     }
 }
 
 player = new Character({x:0,y:0})
 
+const keys={
+    right:{
+        pressed:false
+    },
+    left:{
+        pressed:false
+    },
+    up:{
+        pressed:false
+    },
+    down:{
+        pressed:false
+    }
+
+}
+
 function animate(){
     window.requestAnimationFrame(animate)
 
-    c.fillStyle='black'
-    c.fillRect(0,0,canvas.width,canvas.height)
+    c.clearRect(0,0,canvas.width,canvas.height)
 
     player.update();
+
+    if(keys.right.pressed == true){
+        player.speed.x = 10;
+    } 
+    else if(keys.left.pressed == true){
+        player.speed.x = -10
+    }else player.speed.x = 0;
+
+    if(keys.up.pressed){
+        player.speed.y = -10
+    }
+    
 }
 
+
+
 animate()
+
+addEventListener('keydown',({keyCode})=>{
+    console.log(keyCode)
+
+    switch(keyCode){
+        case 37:
+            keys.left.pressed = true;
+            break;
+        case 39:
+            keys.right.pressed = true;
+            break;
+        case 38:
+            keys.up.pressed = true;
+            break;
+        case 40:
+            break;
+    }
+})
+
+addEventListener('keyup',({keyCode})=>{
+    console.log(keyCode)
+
+    switch(keyCode){
+        case 37:
+            keys.left.pressed = false
+            break;
+        case 39:
+            keys.right.pressed = false
+            break;
+        case 38:
+            keys.up.pressed = false
+            break;
+        case 40:
+            break;
+    }
+})
